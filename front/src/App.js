39ed3854dc70a69e64895617cafe8e7b77146e7b
Daysemotion <kso88794@gmail.com>
2022-07-15 01:45:20 +0900

    Add button components

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import GlobalStyle from './styles/GlobalStyle';
+import Button from './components/modules/Buttons/Button';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/Theme';
 const App = () => {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <h1>asddsa</h1>
+      <Button color={theme.buttonViolet}>dsadas</Button>
+      <Button color={theme.buttonMustard}>dsadas</Button>
+      <Button color={theme.buttonGreen}>dsadas</Button>
+      <Button color={theme.buttonWhite}>dsadas</Button>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    </ThemeProvider>
+  );
 };
 
 export default App;
