34f7626789555d459dea9b86247f7f79cb5b43c2
Daysemotion <kso88794@gmail.com>
2022-07-15 02:06:23 +0900

    Add colorpalete

--- front/src/styles/Theme.js
+++ front/src/styles/Theme.js
@@ -1,8 +1,21 @@
 const theme = {
+  // button color
   buttonViolet: '#877af5',
   buttonGreen: '#13bd7e',
   buttonMustard: '#ffbe4c',
   buttonWhite: '#fff',
+
+  blackLight: '#434444',
+  black: '#000000',
+  strong: '#0b0c0c',
+
+  primary: '#7b7b7b',
+  secondary: '#2b2b2b',
+  tertiary: '#acacac',
+
+  border: '#f5f5ft',
+  borderGreen: '#10df99',
+  hover: '#04c584',
 };
 
 export default theme;
