3651511a769cf0575f63bc63c2809b012b698fab
Daysemotion <kso88794@gmail.com>
2022-07-17 03:42:34 +0900

    Implement Main page

---
+++ front/src/pages/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from '../components/Carousel/index';
+
+const Card = () => {
+  return (
+    <main>
+      <div>
+        <Carousel />
+      </div>
+    </main>
+  );
+};
+
+export default Card;
