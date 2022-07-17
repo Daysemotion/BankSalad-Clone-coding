aec276572e707d835f203403581e415737c53797
Daysemotion <kso88794@gmail.com>
2022-07-18 00:17:53 +0900

    Resive RankingButton

--- front/src/components/modules/Buttons/RankingButton.js
+++ front/src/components/modules/Buttons/RankingButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import theme from '../../../styles/Theme';
 
 const ButtonStyled = styled.button`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  padding: 22px 10px;
+  padding: ${(props) => (props.mobile ? `11px 10px` : `20px 10px`)};
+  margin-left: 16px;
   border-radius: 2px;
   background-color: #91b5be;
   font-size: 14px;
@@ -15,8 +15,8 @@ const ButtonStyled = styled.button`
   color: #fff;
 `;
 
-const RankingButton = () => {
-  return <ButtonStyled>인기카드 더보기</ButtonStyled>;
+const RankingButton = ({ mobile }) => {
+  return <ButtonStyled mobile={mobile}>인기카드 더보기</ButtonStyled>;
 };
 
 export default RankingButton;
